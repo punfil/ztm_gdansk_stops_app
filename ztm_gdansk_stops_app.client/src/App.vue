f763de4624fd12bd6a9fbe650c850e46f89a2f4b
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-12-05 21:31:49 +0100

    Add user stops info component
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- ztm_gdansk_stops_app.client/src/App.vue
+++ ztm_gdansk_stops_app.client/src/App.vue
@@ -52,51 +52,7 @@
             </form>
         </div>
         <!-- List user stops -->
-        <div v-if="this.$store.state.userStops" class="content">
-            <h2>User stops:</h2>
-            <ul>
-                <li v-for="stop in this.$store.state.userStops" :key="stop.stopId">
-                    <strong>Stop ID:</strong> {{ stop.stopId }}<br>
-                    <strong>Last Update:</strong> {{ stop.lastUpdate }}<br>
-                    <strong>Delays:</strong>
-                    <table class="styled-table">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Delay (seconds)</th>
-                                <th>Estimated Time</th>
-                                <th>Headsign</th>
-                                <th>Route Id</th>
-                                <th>Trip Id</th>
-                                <th>Status</th>
-                                <th>Theoretical Time</th>
-                                <th>Timestamp</th>
-                                <th>Trip</th>
-                                <th>Vehicle Code</th>
-                                <th>Vehicle Id</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="delayInfo in stop.delays" :key="delayInfo.id">
-                                <td>{{ delayInfo.id }}</td>
-                                <td>{{ delayInfo.delayInSeconds }}</td>
-                                <td>{{ delayInfo.estimatedTime }}</td>
-                                <td>{{ delayInfo.headsign }}</td>
-                                <td>{{ delayInfo.routeId }}</td>
-                                <td>{{ delayInfo.tripId }}</td>
-                                <td>{{ delayInfo.status }}</td>
-                                <td>{{ delayInfo.theoreticalTime }}</td>
-                                <td>{{ delayInfo.timestamp }}</td>
-                                <td>{{ delayInfo.trip }}</td>
-                                <td>{{ delayInfo.vehicleCode }}</td>
-                                <td>{{ delayInfo.vehicleId }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <button v-on:click="removeFromUserFav(stop.stopId)">Remove from favourite</button>
-                </li>
-            </ul>
-        </div>
+        <user-stops/>
 </template>
 
 <script lang="js">
@@ -150,23 +106,6 @@
                         this.$store.commit('setLoading', false);
                     })
             },
-            
-            removeFromUserFav(stopId) {
-                this.$store.commit('setLoading', true);
-                this.$store.commit('setAllDisplaysNull');
-                fetch(`deletestop/${this.$store.state.loggedIn.id}&${stopId}`, {
-                    method: 'DELETE',
-                })
-                    .then(response => {
-                        this.$store.commit('setLoading', false);
-                        if (!response.ok) {
-                            this.$store.commit('setMsg', "Failed to remove from favourites." + stopId);
-                            return;
-                        }
-
-                        return response.json();
-                    })
-            }
         }
     })
 </script>
--- ztm_gdansk_stops_app.client/src/main.js
+++ ztm_gdansk_stops_app.client/src/main.js
@@ -7,6 +7,7 @@ import Menu from '@/components/Menu.vue'
 import UserList from '@/components/UserList.vue'
 import StopsList from '@/components/StopsList.vue'
 import StopInfo from '@/components/StopInfo.vue'
+import UserStops from '@/components/UserStops.vue'
 
 const app = createApp(App)
 app.use(store)
@@ -14,4 +15,5 @@ app.component('my-menu', Menu)
 app.component('user-list', UserList)
 app.component('stops-list', StopsList)
 app.component('stop-info', StopInfo)
+app.component('user-stops', UserStops)
 app.mount('#app')
