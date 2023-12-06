448af2830d60961bf798bb76f12b134485cb4b8e
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-12-06 23:18:40 +0100

    Fix regression with input type
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- ztm_gdansk_stops_app.client/src/components/DummyInput.vue
+++ ztm_gdansk_stops_app.client/src/components/DummyInput.vue
@@ -4,6 +4,6 @@ defineEmits(['update:modelValue'])
 </script>
 
 <template>
-    <input type="password" :value="modelValue"
+    <input type="text" :value="modelValue"
            @input="$emit('update:modelValue', $event.target.value)" />
 </template>
