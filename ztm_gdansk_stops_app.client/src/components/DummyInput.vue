da0fa8b4329e257734d3f56f72d2cf35a4456087
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-12-06 22:06:13 +0100

    Add two-way data binding in multicomponent
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

---
+++ ztm_gdansk_stops_app.client/src/components/DummyInput.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps(['modelValue'])
+defineEmits(['update:modelValue'])
+</script>
+
+<template>
+    <input type="password" :value="modelValue"
+           @input="$emit('update:modelValue', $event.target.value)" />
+</template>
--- ztm_gdansk_stops_app.client/src/components/LoginForm.vue
+++ ztm_gdansk_stops_app.client/src/components/LoginForm.vue
@@ -1,9 +1,14 @@
+<script setup>
+    import { ref } from 'vue'
+    import DummyInput from './DummyInput.vue'
+</script>
+
 <template>
     <div v-if="this.$store.state.loginMode">
         <h2>Login:</h2>
         <form @submit.prevent="login">
             <label for="username">Username:</label>
-            <input type="text" v-model="username" required>
+            <DummyInput v-model="username" />
             <label for="password">Password:</label>
             <input type="password" v-model="password" required>
             <button type="submit">Login</button>
@@ -13,6 +18,12 @@
 
 <script>
     export default {
+        data() {
+            return {
+                username: '',
+                password: '',
+            };
+        },
         methods: {
             login() {
                 this.$store.commit('setLoading', true);
@@ -24,7 +35,7 @@
                     .then(response => {
                         this.$store.commit('setLoading', false);
                         if (!response.ok) {
-                            this.$store.commit('setMsg', "Failed to login.");
+                            this.$store.commit('setMsg', "Failed to login "+this.username);
                             return;
                         }
 
